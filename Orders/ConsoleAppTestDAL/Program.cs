7d53497ec0e052ced2e057e3dfb13e39cfa8889e
Andres Espinosa <amdresesp21@gmail.com>
2024-07-18 14:42:47 -0500

    feat(back-end): Add the new console of testing of the methods

---
+++ Orders/ConsoleAppTestDAL/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DAL;
+using DAL.Models;
+using System.Linq.Expressions;
+
+
+CreateAsync().GetAwaiter().GetResult();
+RetreiveAsync().GetAwaiter().GetResult();
+
+static async Task CreateAsync()
+{
+    //Add Customer
+    Customer customer = new Customer(){
+        FirstName = "Andres",
+        LastName = "Espinosa",
+        City = "Bogota",
+        Country = "Colombia",
+        Phone = "12345678900"
+    };
+    
+    using(var repository = RepositoryFactory.CreateRepository())
+    {
+        try
+        {
+            var createCustomer = await repository.CreateAsync(customer);
+            Console.WriteLine($"Add Customer: {createCustomer.LastName} {createCustomer.FirstName}");
+        }
+        catch ( Exception ex )
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+
+static async Task RetreiveAsync ()
+{
+    using (var repository = RepositoryFactory.CreateRepository()) 
+    {
+        try
+        {
+            Expression<Func<Customer, bool>> criteria = c => c.FirstName == "Andres" && c.LastName == "Espinosa";
+            var customer = await repository.RetreiveAsync(criteria);
+            if (customer == null)
+            {
+                Console.WriteLine($"Retrived Customer: {customer.FirstName} \t{customer.LastName} \tCity: {customer.City} \tCountry: {customer.Country}");
+            }
+            else
+            {
+                Console.WriteLine("Customer not exist");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}

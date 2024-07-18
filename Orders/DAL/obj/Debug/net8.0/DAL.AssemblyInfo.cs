f824dcb8462ea4e44c523a518e3ffe7e2cef5bc8
Andres Espinosa <amdresesp21@gmail.com>
2024-07-18 16:35:09 -0500

    feat(back-End): add the files for correct operation

--- Orders/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ Orders/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d53497ec0e052ced2e057e3dfb13e39cfa8889e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e512f1c93544940953c6cf16b6411ce895267a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Orders/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Orders/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d53497ec0e052ced2e057e3dfb13e39cfa8889e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e512f1c93544940953c6cf16b6411ce895267a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

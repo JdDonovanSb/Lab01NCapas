64c3552950177eb2c695a1c48d395ad6a74e38b1
Andres Espinosa <amdresesp21@gmail.com>
2024-07-30 15:50:43 -0500

    feat(back-end): Add proyect ProxyServer, Class CustomerProxy with his methods and interface ICustomerProxy

---
+++ Orders/ProxyServices/Interfaces/IcustomerProxy.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyServices.Interfaces
+{
+    public interface ICustomerProxy
+    {
+        Task<Customer> CreateAsync(Customer customer);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<List<Customer>> GetAllAsync();
+
+        Task<Customer> GetByIdAsync(int id);
+
+        Task<bool> UpdateAsync(int id, Customer customer);
+
+    }
+}

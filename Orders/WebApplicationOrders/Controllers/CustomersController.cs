44e4dae69bdbd7b333d902357133f98b45389f08
Andres Espinosa <amdresesp21@gmail.com>
2024-08-01 17:37:15 -0500

    feat(Back-End):Add the back-end code of the following files for its correct functioning: Orders/WebApplicationOrders/Controllers/CustomersController., Orders/WebApplicationOrders/Views/Shared/_Layout.cshtml, Orders/WebApplicationOrders/WebApplicationOrders.csproj

--- Orders/WebApplicationOrders/Controllers/CustomersController.cs
+++ Orders/WebApplicationOrders/Controllers/CustomersController.cs
@@ -18,15 +18,13 @@ namespace WebApplicationOrders.Controllers
             var customers = await _proxy.GetAllAsync();
             return View(customers);
         }
-
         //Create
-        // GET: Customer/Create
+            // GET: Customer/Create
         public ActionResult Create()
         {
             return View();
         }
-
-        // POST: Customer/Create
+            // POST: Customer/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
 
@@ -49,6 +47,91 @@ namespace WebApplicationOrders.Controllers
                 }
             }
             return View(customer);
+        }
+        //Edit
+            //GET: Customer/Edit/5
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var customer = await _proxy.GetByIdAsync(Id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+            // POST: Customer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FirstName,LastName, City, Country, Phone")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _proxy.UpdateAsync(id, customer);
+                    if (!result)
+                    {
+                        return RedirectToAction("Error", new { message = "No se puede realizar la edición porque hay duplicidad de nombre con otro cliente." });
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    return RedirectToAction("Error", new { message = ex.Message });
+                }
+            }
+            return View(customer);
+        }
+        //Details
+            //GET: /Customer/Detais/5
+         public async Task<IActionResult> Details(int Id)
+         {
+            var customer = await _proxy.GetByIdAsync(Id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+         }
+        //Delete
+        //GET: Customer/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var customer = await _proxy.GetByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+        //POST Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                var result = await _proxy.DeleteAsync(id);
+                if (!result)
+                {
+                    return RedirectToAction("Error", new { message = "No se puede liminar el cliente porque tiene facturas asociadas. " });
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error", new {message = ex.Message});
+            }
+        }
+        //Error
+        public IActionResult Error(string message)
+        {
+            ViewBag.ErrorMessage = message;
+            return View();
         }
     }
 }

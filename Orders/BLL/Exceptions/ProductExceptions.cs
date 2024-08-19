981ae2fe7c0390d4c18ea85e93b86777655c7647
JdDonovanSb <donovanboni@gmail.com>
2024-08-19 16:03:04 -0500

    feat(back-end): Add the new class and exceptions 'Supplier', Product, 'SupplierExceptions' and 'ProductExceptions'

---
+++ Orders/BLL/Exceptions/ProductExceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ProductExceptions : Exception
+    {
+        private ProductExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowProductAlreadyExistsException(string productName)
+        {
+            throw new ProductExceptions($"A product with the name already exists {productName}.");
+        }
+
+        public static void ThrowInvalidProductDataException(string message)
+        {
+            throw new ProductExceptions(message);
+        }
+
+        public static void ThrowInvalidProductIdException(int id)
+        {
+            throw new ProductExceptions($"The product with id {id} was not found or dont exist");
+        }
+    }
+}
---
+++ Orders/BLL/Exceptions/SupplierExceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class SupplierExceptions : Exception
+    {
+        private SupplierExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowSupplierAlreadyExistsException(string companyName)
+        {
+            throw new SupplierExceptions($"A supplier with the name already exists {companyName}.");
+        }
+
+        public static void ThrowInvalidSupplierDataException(string message)
+        {
+            throw new SupplierExceptions(message);
+        }
+
+        public static void ThrowInvalidSupplierIdException(int id)
+        {
+            throw new SupplierExceptions($"The client with id {id} was not found or dont exist.");
+        }
+    }
+}

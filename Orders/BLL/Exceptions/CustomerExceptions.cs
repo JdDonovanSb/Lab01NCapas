badb828f939f4ea18b9225c995c3d7422d225248
JdDonovanSb <donovanboni@gmail.com>
2024-08-22 09:52:29 -0500

    feat(back-end): Modify file 'BLL/Exceptions/CustomerExceptions.cs'

--- Orders/BLL/Exceptions/CustomerExceptions.cs
+++ Orders/BLL/Exceptions/CustomerExceptions.cs
@@ -13,7 +13,7 @@ namespace BLL.Exceptions
 
         }
 
-        public static void ThrowSupplierAlreadyExistsException(string firstName, string lastName)
+        public static void ThrowCustomerAlreadyExistsException(string firstName, string lastName)
         {
             throw new CustomerExceptions($"A client with the name already exists {firstName} {lastName}.");
         }
